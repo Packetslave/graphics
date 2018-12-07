7fc22650cca619812f13eca6e17725182e36888e
Brian Landers <brian@packetslave.com>
2018-12-06 22:50:08 -0800

    add file watcher

--- viewer/viewer.cpp
+++ viewer/viewer.cpp
@@ -2,29 +2,62 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <iostream>
 
-using namespace cv;
-using namespace std;
+#include <boost/filesystem.hpp>
+#include "efsw/efsw.hpp"
+
+class Image : public efsw::FileWatchListener {
+public:
+    explicit Image(std::string filename);
+
+    void display() {
+        cv::Mat image;
+        image = cv::imread(path_.c_str());
+
+        if(! image.data )
+        {
+            std::cout <<  "Could not open or find the image" << std::endl ;
+            return;
+        }
+
+        // FIXME: make the window redisplay in the right place
+        namedWindow( "Display window", cv::WINDOW_AUTOSIZE );
+        imshow( "Display window", image );
+    }
+
+    void show() {
+        watcher_->addWatch(path_.parent_path().c_str(), this, false);
+        this->display();
+    }
+
+    // FIXME: clean up the lint warning
+    void handleFileAction(efsw::WatchID watchid, const std::string& dir, const std::string& filename, efsw::Action action, std::string oldFilename = "" ) override
+    {
+        if (filename == path_.filename()) {
+            std::cout << "DIR (" << dir << ") FILE (" << filename << ") has event " << action << std::endl;
+            this->display();
+        }
+    }
+
+private:
+    boost::filesystem::path path_;
+    std::unique_ptr<efsw::FileWatcher> watcher_;
+};
+
+Image::Image(std::string filename) {
+    path_ = boost::filesystem::path(filename);
+    watcher_ = std::make_unique<efsw::FileWatcher>();
+}
 
 int main( int argc, char** argv )
 {
     if( argc != 2)
     {
-        cout <<" Usage: display_image ImageToLoadAndDisplay" << endl;
+        std::cout <<" Usage: display_image directory imageImageToLoadAndDisplay" << std::endl;
         return -1;
     }
+    auto i = Image(argv[1]);
+    i.show();
+    cv::waitKey(0);
 
-    Mat image;
-    image = imread(argv[1]);   // Read the file
-
-    if(! image.data )                              // Check for invalid input
-    {
-        cout <<  "Could not open or find the image" << std::endl ;
-        return -1;
-    }
-
-    namedWindow( "Display window", WINDOW_AUTOSIZE );// Create a window for display.
-    imshow( "Display window", image );                   // Show our image inside it.
-
-    waitKey(0);                                          // Wait for a keystroke in the window
     return 0;
 }
